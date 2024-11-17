45da514c3ab8b23ec6711f6b7348d5d81f0719b8
lauratellez321 <lauravalentc@gmail.com>
2024-11-16 21:05:04 -0500

    Ajuste componente header y pdf

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { PdfViewerModule } from "ng2-pdf-viewer";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { HomeComponent } from "./core/home/home.component";
-import { PdfComponent } from './core/pdf/pdf.component';
+import { PdfComponent } from "./core/pdf/pdf.component";
+import { HeaderComponent } from "./core/header/header.component";
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent, PdfComponent],
+  declarations: [AppComponent, HomeComponent, PdfComponent, HeaderComponent],
   imports: [BrowserModule, AppRoutingModule, PdfViewerModule],
   providers: [],
   bootstrap: [AppComponent],
